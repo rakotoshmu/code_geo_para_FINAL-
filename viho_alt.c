b47ee8c057f7db160d0cc3d8f2d70dee9522a007
hugomoe <hugo.moeneclaey@free.fr>
2015-06-25 19:08:04 +0200

    Update viho_alt.c

--- viho_alt.c
+++ viho_alt.c
@@ -1,5 +1,7 @@
-#include "iio.h"
-#include "ftr.h"
+//gcc-5 -fopenmp -O3 viho.c -I/usr/local/include/libiomp -I/usr/X11/include -I/Users/Hhhh/ENS/Stage_L3_math/homographies/code/jpeg-6b -L/usr/X11/lib -lfftw3 -lX11 -L/usr/local/Cellar/libtiff/4.0.3 -ltiff -ljpeg -lpng
+
+
+#include "iio.c"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
